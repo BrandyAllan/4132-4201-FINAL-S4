58a203f6317bda2fa2cbb59e16a08fbc20dc8ef9
Hotiana <rasamizafyhotiana@gmail.com>
2026-07-20 09:33:54 +0300

    prefixe modele

---
+++ app/Models/PrefixeModel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PrefixeModel extends Model
+{
+    protected $table            = 'prefixes_operateur';
+    protected $primaryKey       = 'id';
+    protected $returnType       = 'array';
+    protected $allowedFields    = ['prefixe', 'actif'];
+
+
+    public function recupererPrefixesActifs(): array
+    {
+        $resultats = $this->where('actif', 1)->findAll();
+        return array_column($resultats, 'prefixe');
+    }
+
+
+    public function estPrefixeValide(string $prefixe): bool
+    {
+        $check = $this->where('prefixe', $prefixe)
+                      ->where('actif', 1)
+                      ->first();
+                      
+        return $check !== null;
+    }
+}
